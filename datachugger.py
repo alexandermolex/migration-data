36427dd2623c914b5e61a0a94b126c5c910d7ae3
Alex M <alexandermolex@yahoo.com>
2026-02-12 18:15:13 -0500

    Update datachugger.py
    added a default value for prompt

--- datachugger.py
+++ datachugger.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import os
 
-def select_file(prompt):
+def select_file(prompt = "Select a file"):
     filepath = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv"), ("Excel files", "*.xlsx;*.xls")], title = prompt )
     if filepath.split('.')[-1] in ['xlsx', 'xls']:
         df = pd.read_excel(filepath)
@@ -14,7 +14,6 @@ def select_file(prompt):
         df = pd.read_csv(filepath)
         # You can display the DataFrame here, or do something else with it
         print(df.head()) # Print the first few rows of the DataFrame
-
 
 inflow_05_09 = select_file("Select the inflow data file for 05-09")
 outflow_05_09 = select_file("Select the outflow data file for 05-09")

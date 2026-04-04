8e7ac6d93793a70a0bc6fb6f2b09c7899148465e
Alex M <alexandermolex@yahoo.com>
2026-04-03 20:59:14 -0400

    Visuals Commit
    I honestly took a long break from this code. I will finish it so that the visuals can be created and finished but I am pivoting the narrative in order to include a larger dataset that extends all the way to 2025.

--- visuals.py
+++ visuals.py
@@ -2,4 +2,20 @@ import sqlite3
 import pandas as pd
 import pathlib
 from pathlib import Path
+# Setup to connect to the Full Database of Clean Data
+with sqlite3.connect('Full_Migration.db') as conn:
+    cursor = conn.cursor()
+    
+    #This SQL command selects all the relevant data for states population data
+    # To default for now it is set to michgian but I plan to expand it to a loop
+    # That way I can run it to make visuals for every state
+
+    cursor.execute(
+        """select DISTINCT origin_state_name, origin_county_name, county_of_current_residence_estimate, origin_nonmovers_estimate, origin_movers_us_estimate, year
+        from inflow 
+        where origin_state_name = "Michigan"
+        order by year,  origin_fips_county, dest_fips_state,dest_fips_county desc;
+        """)
+    pop_est = cursor.fetchall()
+    
 
